ffa2e0f5643ac039f3423514a686dcd457791b7d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-08-16 22:45:35 +0200

    Install Firebase Connect to Firebase Project

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, serverTimestamp } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB2lev5JPqjxErFkgrzvNebBdcwC9FMS8M",
+    authDomain: "vue-3-live-chat-d7d81.firebaseapp.com",
+    projectId: "vue-3-live-chat-d7d81",
+    storageBucket: "vue-3-live-chat-d7d81.firebasestorage.app",
+    messagingSenderId: "165816885529",
+    appId: "1:165816885529:web:cb7774d77fa37b5aca6588"
+};
+
+// Initialize Firebase, Firestore and Timestamp
+const firebase = initializeApp(firebaseConfig);
+const projectFirestore = getFirestore(firebase);
+const projectTimestamp = serverTimestamp;
+
+export { projectFirestore, projectTimestamp };
+
